86fcdb62e72f04d4db332a7480b8f6dbdbe866ae
Regb18 <reginald.ac.barnes@gmail.com>
2023-01-18 10:16:20 -0500

    +logo, dry colors, tern colors, buttons

--- js/site.js
+++ js/site.js
@@ -47,42 +47,83 @@ function displayFizzBuzz(array, fizz, buzz) {
     let tableBody = document.getElementById('results');
     let tableHTML = ""
     
-/*    for (i = 0; i < array.length; i++) {
+   for (i = 0; i < array.length; i++) {
 
         let value = array[i];
-        
-    Ternary operator - Working Answer # 2
-        let className = 
-        value % fizz == 0 && value % buzz == 0 ? value = 'FizzBuzz' :
-        value % fizz == 0 ? value = 'Fizz' :
-        value % buzz == 0 ? value = 'Buzz' : 
-        value;
-*/      
-/*  Working Answer # 1
         let className = ""
 
         if (value % fizz == 0 && value % buzz == 0) {
             // output FizzBuzz class
             value = 'FizzBuzz';
-            className = 'fizzBuzzClass'
+            className = 'fizzBuzz'
 
         } else if (value % fizz == 0) {
             // output Fizz class
             value = 'Fizz'
-            className = 'fizzClass'
+            className = 'fizz'
 
         } else if (value % buzz == 0) {
             // output Buzz class
             value = 'Buzz'
-            className = 'buzzClass'
+            className = 'buzz'
 
         } else {
             className = 'neither'
         }
 
-*/
+        if (i % 5 == 0) {
+            tableHTML += "<tr>"
+        }
+        
+        let tableRow = `<td class="${className}">${value}</td>`; 
+        tableHTML += tableRow; 
+        
+        if ((i + 1) % 5 == 0) {
+            tableHTML += "</tr>" 
+        } 
+        
+
+    }
+
+    tableBody.innerHTML = tableHTML;
+}
+
+// Ternary Solution
+function displayFizzBuzzTern(array, fizz, buzz) {
+
+    let tableHTML = ""
+    
+    for (i = 0; i < array.length; i++) {
+
+        let value = array[i];
+        
+        let className = 
+        value % fizz == 0 && value % buzz == 0 ? value = 'FizzBuzz' :
+        value % fizz == 0 ? value = 'Fizz' :
+        value % buzz == 0 ? value = 'Buzz' : 
+        value;
+
+        i % 5 == 0 ? tableHTML += "<tr>" :
+        value;
+        
+        tableHTML += `<td class="${className}">${value}</td>`;
+        
+        (i + 1) % 5 == 0 ? tableHTML += "</tr>" :
+        value;
+    }
+
+    document.getElementById('results').innerHTML = tableHTML;
+}
+      
+
+
 
 // / DRY solution
+
+function displayFizzBuzzDry(array, fizz, buzz) {
+
+    let tableHTML = "";
+
     for (i = 0; i < array.length; i++) {
 
         let value = array[i];
@@ -92,13 +133,13 @@ function displayFizzBuzz(array, fizz, buzz) {
 
         if (isDivisiblebyFizz && isDivisiblebyBuzz) {
                 value = 'FizzBuzz';
-                className = 'fizzBuzz';
+                className = 'fizzBuzzDry';
         } else if (isDivisiblebyFizz) {
                 value ='Fizz'
-                className = 'fizz';
+                className = 'fizzDry';
         } else if (isDivisiblebyBuzz) {
                 value = 'Buzz';
-                className = 'buzz';            
+                className = 'buzzDry';            
         }   
 
 
@@ -112,6 +153,59 @@ function displayFizzBuzz(array, fizz, buzz) {
 
     }
 
-    tableBody.innerHTML = tableHTML;
+    document.getElementById('results').innerHTML = tableHTML;
+
+}
+
+
+
+
+
+
+function getValuesTern() {
+    let fizzValue = parseInt(document.getElementById('fizzValue').value)
+    let buzzValue = parseInt(document.getElementById('buzzValue').value)
+    let stopValue = parseInt(document.getElementById('stopValue').value)
+
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue)) {
+
+        let numberArray = generateFizzBuzz(stopValue);
+
+        displayFizzBuzzTern(numberArray, fizzValue, buzzValue);
+
+    } else {
+        // if not, tell our user
+        Swal.fire(
+            {
+                icon: 'error',
+                title: 'Oops!',
+                text: 'Only integers are allowed for FizzBuzz!'
+            }
+        )
+    }
+
+}
+
+function getValuesDry() {
+    let fizzValue = parseInt(document.getElementById('fizzValue').value)
+    let buzzValue = parseInt(document.getElementById('buzzValue').value)
+    let stopValue = parseInt(document.getElementById('stopValue').value)
+
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue)) {
+
+        let numberArray = generateFizzBuzz(stopValue);
+
+        displayFizzBuzzDry(numberArray, fizzValue, buzzValue);
+
+    } else {
+        // if not, tell our user
+        Swal.fire(
+            {
+                icon: 'error',
+                title: 'Oops!',
+                text: 'Only integers are allowed for FizzBuzz!'
+            }
+        )
+    }
 
 }
